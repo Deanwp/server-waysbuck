92e06f7e4b543d240ce9c06c9090ecf6322400bd
unknown <deanwahyu12@gmail.com>
2022-04-06 02:54:24 +0700

    first commit

--- seeders/20220405184048-admin.js
+++ seeders/20220405184048-admin.js
@@ -20,7 +20,7 @@ module.exports = {
           password:
             '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
           name: 'admin',
-          status: 'admin',
+          role: 'admin',
         },
       ],
       {}
